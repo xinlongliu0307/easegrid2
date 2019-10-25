d874c6e0156dccefb26f13f8883bbdd0fe6a289c
Kat Jensen <kat.m.jensen@gmail.com>
2019-10-24 21:55:29 -0400

    updated documentation

--- easegrid2.py
+++ easegrid2.py
@@ -1,47 +1,83 @@
 #! /usr/bin/python
-##############################################
+
+#########################################################################################################
+# easegrid2.py
+#
+# Geolocation of grid center points for large scale raster images with EASEGrid projection
+#
+# See description of EASEGrid convention: https://nsidc.org/ease/ease-grid-projection-gt
+
+# Available grids include:
+#   - Global Version 1 (M1)
+#   - Global Version 2 (M2)
+#   - Polar Northern Hemisphere Version 1 (N1)
+#   - Polar Northern Hemisphere Version 2 (N2)
+#   - Polar Southern Hemisphere Version 1 (S1)
+#   - Polar Southern Hemisphere Version 2 (S2)
+#
+# Available spatial resolutions:
+#   - 1 km
+#   - 3 km
+#   - 3.125 km
+#   - 6.25 km
+#   - 9 km
+#   - 12.5 km
+#   - 25 km
+#   - 36 km
+#
+# Each unique projection is defined by its own class (e.g. M1, M2 ... etc)
+# Requiring one input parameter: a str representation of the spatial resolution (e.g. 'g01', 'g03'...)
+#
+# Example usage:
+#   * EASEGrid Version 2 Global 3km instance: grid = M1('g03')
+#   * EASEGrid Version 1 Polar Northern Hemisphere 25km instance: grid = P1('g25')
+#
 # Requires: py-yaml, numpy, pyproj
-##############################################
+#########################################################################################################
+
+__author__ = 'kat jensen'
 
 import os
 import sys
 from collections import namedtuple
-
 import yaml
 import numpy as np
 from pyproj import Proj
 
 class _Grid(object):
-    '''EASEGrid grid object template.
+    """
+    EASEGrid grid object template.
 
     Keywords:
-    grid_name -- Subgrid name (e.g. g09, g03) 
-
-    '''
+    grid_name -- Subgrid name (e.g. 'g09', 'g03')
+    """
     _src_path = os.path.split(os.path.abspath(__file__))[0]
     _dat_path = os.path.join(_src_path, 'dat')
     _constant = yaml.load(open(os.path.join(_src_path, 'grids.yaml'), 'r').read())
     coord_scale = 1e-05
 
     def __init__(self, grid_name):
-        # get grid name
+        # get grid name, check it is an accepted grid type
         try:
             assert grid_name in self.grid_names
-        except:
+        except AssertionError:
             raise Exception('Valid grid name in {0}'.format(self.grid_names))
         self.grid_name = grid_name
+
         # set the projection
         self.p = self.p_args
+
         # set the grid
         _constant = namedtuple('grid_constant', 'size cols rows r0 s0')
         self.constant = _constant(**self._constant[self.name][grid_name])
+
         # id string unique to grid/proj combinatio
         self.grid_id = self.name + '_' + getattr(self, 'hemi', 'G') + '_' + self.grid_name
         
-    '''
+    """
     Properties
     ~~~~~~~~~~
-    '''
+    """
 
     @property
     def p(self):
@@ -49,13 +85,13 @@ class _Grid(object):
 
     @p.setter
     def p(self, val):
-        ''' Proj4 object.'''
+        """ Proj4 object."""
         val = dict(zip(val, [getattr(self, v, None) for v in val]))
         self._p = Proj(val)
 
     @property
     def geotransform(self):
-        ''' Affine transform array.'''
+        """ Affine transform array."""
         return [self.constant.s0, self.constant.size, 0, self.constant.r0, 0, self.constant.size]
 
     @property
@@ -79,20 +115,20 @@ class _Grid(object):
         self._latitude = lat
         self._longitude = lon
 
-    '''
+    """
     Methods
     ~~~~~~~
-    '''
+    """
 
     FILES = {'g25': 'L', 'g12': 'H'}
     def from_file(self):
-        ''' Not available for all files. '''
+        """ Read in pre-calculated grid coordinates from file, if existent """
         assert self.name[1] == '1'
         assert self.grid_name in self.FILES
         lat_file, lon_file = self._coord_file()
         lat = self._read_coord(lat_file)
         lon = self._read_coord(lon_file)
-        return lon, lat 
+        return lon, lat
 
     def _coord_file(self):
         coord_ = self.hemi + self.FILES[self.grid_name]
@@ -102,17 +138,17 @@ class _Grid(object):
         file_name = os.path.join(self._dat_path, file_name)
         coord = np.fromfile(file_name, dtype='i')
         coord.resize((self.constant.rows, self.constant.cols))
-        return coord * self.coord_scale        
-                    
+        return coord * self.coord_scale
+
     def from_array(self):
         one_=np.array(1)
         s_, r_ = np.meshgrid(np.arange(1, self.constant.cols + 1), np.arange(1, self.constant.rows + 1))
         return self.inverse(s_, r_)
 
     def forward(self, lon, lat):
-        '''Lat/lon forward projected to raster/scan.
+        """Lat/lon forward projected to raster pixel coords
         note: one-based index 
-        '''
+        """
         x_, y_ = self.p(lon, lat)
         assert self.units == 'm'
         dR, dS = x_/self.constant.size, -y_/self.constant.size 
@@ -121,26 +157,26 @@ class _Grid(object):
         return r_, s_
     
     def inverse(self, r_, s_):
-        '''Raster/scan inverse projected to lat/lon.
-        note: one-based index 
-        '''
+        """ Raster pixel coords inverse projected to lat/lon.
+        note: one-based index
+        """
         assert self.units == 'm'
         x_ = (r_ - self.constant.r0 - 1) * self.constant.size
         y_ = (s_ - self.constant.s0 - 1) * -self.constant.size
         lon, lat = self.p(x_, y_, inverse=True)
         return lon, lat
 
-'''
+"""
 Global Cylindrical
 ~~~~~~~~~~~~~~~~~~
-'''
+"""
 
 class M1(_Grid):
-    ''' EASEGrid ver1 Global, an equal area cylindrical projection.
-        
+    """ EASEGrid version1 Global, an equal area cylindrical projection.
+
     Keywords:
     grid_name -- Name of grid {'l', 'h'}
-    '''
+    """
     name = 'M1'
     hemi = 'M'
     proj = 'cea'
@@ -153,11 +189,11 @@ class M1(_Grid):
     grid_names = _Grid._constant[name].keys()
 
 class M2(_Grid):
-    ''' EASEGrid ver1 Global, an equal area cylindrical projection.
+    """ EASEGrid version2 Global, an equal area cylindrical projection.
         
     Keywords:
     grid_name -- Name of grid {'l', 'h'}
-    '''
+    """
     name = 'M2'
     hemi = 'M'
     proj = 'cea'
@@ -172,10 +208,10 @@ class M2(_Grid):
     p_args = ['proj', 'lat_0', 'lon_0', 'lat_1', 'x_0', 'y_0', 'ellps', 'datum', 'units']
     grid_names = _Grid._constant[name].keys()
 
-'''
+"""
 Polar Azimuthal
 ~~~~~~~~~~~~~~~
-'''
+"""
 
 class P2(_Grid):
     name = 'P2'
@@ -190,10 +226,12 @@ class P2(_Grid):
     grid_names = _Grid._constant[name].keys()
 
 class S2(P2):
+    """ EASEGrid version 2 Polar - Southern Hemisphere"""
     lat_0 = -90
     hemi = 'S'
     
 class N2(P2):
+    """ EASEGrid version 2 Polar - Northern Hemisphere"""
     lat_0 = 90
     hemi = 'N'
 
@@ -209,9 +247,11 @@ class P1(_Grid):
     grid_names = _Grid._constant[name].keys()
 
 class S1(P1):
+    """ EASEGrid version 1 Polar - Southern Hemisphere"""
     lat_0 = -90
     hemi = 'S'
 
 class N1(P1):
+    """ EASEGrid version 1 Polar - Northern Hemisphere"""
     lat_0 = 90
     hemi = 'N'
